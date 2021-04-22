1a1181d7487f1702bb9ec772f818c4f354d23504
hokli <2629910752@qq.com>
2021-04-22 11:01:36 +0800

    1:

--- src/com/lwoo/test/j8/OptionalDemo.java
+++ src/com/lwoo/test/j8/OptionalDemo.java
@@ -1,6 +1,20 @@
-package com.lwoo.test.j8;/**
- * @author  hokli
- * @date  2021/1/27 15:32
+package com.lwoo.test.j8;
+
+import java.util.Optional;
+
+/**
+ * @author hokli
  * @version 1.0
- */public class OptionalDemo {
+ * @date 2021/1/27 15:32
+ */
+public class OptionalDemo {
+
+    public static void main(String[] args) {
+
+        //调用工厂方法创建Optional实例
+        Optional<String> name = Optional.of("Sanaulla");
+        System.out.println(name.isPresent());
+        //传入参数为null，抛出NullPointerException.
+        Optional<String> someNull = Optional.of(null);
+    }
 }
